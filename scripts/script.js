166acd311902e55f5793974850416060810b0e1b
Allan Andrade de Jesus <42648038+Allan93aj@users.noreply.github.com>
2024-12-22 18:03:22 -0300

    meu cv

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const backToTop = document.querySelector('.back-to-top');
+    const whatsapp = document.querySelector('.whatsapp');
+
+    backToTop.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    whatsapp.addEventListener('click', () => {
+        window.open('https://wa.me/5551964299127', '_blank');
+    });
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            backToTop.style.display = 'flex';
+        } else {
+            backToTop.style.display = 'none';
+        }
+    });
+});
+
+function redirectTo(link) {
+    window.open(link, '_blank');
+}
